cbd3f5deb8a0fb5d7a6334c27479e4b104114fa7
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-06-12 23:45:00 +0800

    fix

--- quora-show-answers-by-default.js
+++ quora-show-answers-by-default.js
@@ -3,21 +3,41 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  Show answers by default instead of 'all related' answers
 // @match        https://www.quora.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @updateURL    https://github.com/johan456789/userscripts/raw/main/quora-show-answers-by-default.js
+// @downloadURL  https://github.com/johan456789/userscripts/raw/main/quora-show-answers-by-default.js
 // ==/UserScript==
 
 
 (function() {
     'use strict';
     console.log('running');
-    
-    waitForKeyElements("#mainContent > div:nth-child(2) > div > div.q-inlineFlex.qu-alignItems--flex-end.DesktopPopoverMenu___StyledInlineFlex-sc-47js9m-0.hfwRBf > div > div > button > div", click);
-    waitForKeyElements("#mainContent > div:nth-child(2) > div > div.q-inlineFlex.qu-alignItems--flex-end.DesktopPopoverMenu___StyledInlineFlex-sc-47js9m-0.hfwRBf > div > div.q-box.qu-zIndex--popover > div > div.q-box.qu-bg--raised.qu-borderRadius--small.qu-borderAll.qu-borderColor--gray.qu-overflow--hidden.qu-boxShadow--large > div > div > div:nth-child(2)", click);
+
+    const dropdownBtnSelector = '#mainContent > div.q-box button';
+    const dropdownMenuSelector = '.q-box .puppeteer_test_popover_menu';
+
+    waitForKeyElements(dropdownBtnSelector, click);
+    waitForKeyElements(dropdownMenuSelector, callback);
 
     console.log('done');
+    
+    function callback(jNode) {
+        // Find element containing "answer" text (case insensitive)
+        const answerElement = jNode.find('*').filter(function() {
+            return $(this).text().toLowerCase().includes('answer');
+        }).first();
+        
+        if (answerElement.length) {
+            console.log('Found answer element, clicking...');
+            answerElement.click();
+        } else {
+            console.log('Answer element not found');
+        }
+    }
+    
     function click(jNode) {
         jNode.click();
     }
