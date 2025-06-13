2d2367ad61ab9962a28937425233986df7f6e074
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-06-13 23:18:11 +0800

    fix: infinite loop

--- quora-show-answers-by-default.js
+++ quora-show-answers-by-default.js
@@ -1,44 +1,66 @@
 // ==UserScript==
 // @name         Quora-Show-Answers-By-Default
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
-// @description  Show answers by default instead of 'all related' answers
+// @version      0.4.0
 // @match        https://www.quora.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @updateURL    https://github.com/johan456789/userscripts/raw/main/quora-show-answers-by-default.js
 // @downloadURL  https://github.com/johan456789/userscripts/raw/main/quora-show-answers-by-default.js
 // ==/UserScript==
 
-
-(function() {
+(function () {
     'use strict';
-    console.log('running');
+    console.log('Quora-Show-Answers-By-Default: script started');
 
     const dropdownBtnSelector = '#mainContent .q-box button';
     const dropdownMenuSelector = '.q-box .puppeteer_test_popover_menu';
 
-    waitForKeyElements(dropdownBtnSelector, click);
-    waitForKeyElements(dropdownMenuSelector, callback);
+    // Delay (ms) between clicking the dropdown button and selecting the "answer" option.
+    const SECOND_CLICK_DELAY_MS = 300;
 
-    console.log('done');
-    
-    function callback(jNode) {
-        // Find element containing "answer" text (case insensitive)
-        const answerElement = jNode.find('*').filter(function() {
-            return $(this).text().toLowerCase().includes('answer');
-        }).first();
-        
-        if (answerElement.length) {
-            console.log('Found answer element, clicking...');
-            answerElement.click();
-        } else {
-            console.log('Answer element not found');
+    waitForElement(dropdownBtnSelector, clickDropdownBtn);
+    waitForElement(dropdownMenuSelector, clickAnswerInMenu);
+
+    /**
+     * Observe the document until an element that matches `selector` exists, then
+     * invoke `callback(el)` and stop observing.
+     * @param {string} selector CSS selector for target element.
+     * @param {(el: HTMLElement) => void} callback Function to run when found.
+     */
+    function waitForElement(selector, callback) {
+        const existing = document.querySelector(selector);
+        if (existing) {
+            callback(existing);
+            return;
         }
+
+        const observer = new MutationObserver(() => {
+            const el = document.querySelector(selector);
+            if (el) {
+                observer.disconnect();
+                callback(el);
+            }
+        });
+
+        observer.observe(document.body, { childList: true, subtree: true });
     }
-    
-    function click(jNode) {
-        jNode.click();
+
+    function clickDropdownBtn(btn) {
+        console.log('Quora-Show-Answers-By-Default: clicking dropdown button');
+        btn.click();
+    }
+
+    function clickAnswerInMenu(menu) {
+        setTimeout(() => {
+            const answerBtn = Array.from(menu.querySelectorAll('*'))
+                .find((el) => el.textContent.toLowerCase().includes('answer'));
+
+            if (answerBtn) {
+                console.log('Quora-Show-Answers-By-Default: clicking "answer" option');
+                answerBtn.click();
+            } else {
+                console.log('Quora-Show-Answers-By-Default: "answer" option not found');
+            }
+        }, SECOND_CLICK_DELAY_MS);
     }
 })();
