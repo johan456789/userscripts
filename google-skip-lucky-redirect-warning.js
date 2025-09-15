1e879946631dc7c87ee3dcd24111ba6313d59182
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-09-15 10:48:19 +0800

    fix: remove redundant code and increase speed

--- google-skip-lucky-redirect-warning.js
+++ google-skip-lucky-redirect-warning.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Skip Redirect Warning
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Skip Google's redirection notice (google.com/url?q=*) and go directly to the target link
 // @author       You
 // @match        https://www.google.com/url*
@@ -12,39 +12,13 @@
 // @downloadURL  https://github.com/johan456789/userscripts/raw/main/google-skip-lucky-redirect-warning.js
 // ==/UserScript==
 
-function logger(message) {
-    console.log("[Google-Skip-Redirect] " + message);
-}
+//function logger(message) {
+//    console.log("[Google-Skip-Redirect] " + message);
+//}
 
-(function () {
-    'use strict';
-
-    const logger = window.__G_SKIP_REDIRECT_LOGGER__ || ((msg) => console.log("[Google-Skip-Redirect] " + msg));
-    logger('Script started');
-
-    try {
-        const currentUrl = new URL(window.location.href);
-        if (currentUrl.hostname === 'www.google.com' && currentUrl.pathname === '/url') {
-            const params = currentUrl.searchParams;
-            const target = params.get('q');
-
-            if (target && typeof target === 'string') {
-                // Avoid loops if q points back to Google's redirector
-                if (!/^https?:\/\/www\.google\.com\/url/i.test(target)) {
-                    // Redirect immediately without adding to history
-                    logger('Redirecting to target: ' + target);
-                    window.location.replace(target);
-                }
-                else {
-                    logger('Target is another Google redirect URL. Skipping to avoid loop.');
-                }
-            } else {
-                logger('No target found in q or url parameter.');
-            }
-        }
-    } catch (e) {
-        logger('Error occurred: ' + (e && e.message ? e.message : String(e)));
-    }
+(() => {
+  const u = new URL(window.location.href);
+  const q = u.searchParams.get("q");
+  window.location.replace(q);
+  //logger('redirected');
 })();
-
-
