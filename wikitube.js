07a53dc2fe00c596fb623ee05614380a7cf698a5
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-09-16 20:52:56 +0800

    refactor: improve cohesion and modularity

--- wikitube.js
+++ wikitube.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   Wikitube - YouTube on 维基百科 & 百度百科
 // @name:zh-TW   Wikitube - YouTube on 維基百科 & 百度百科
 // @namespace    thyu
-// @version      3.6.7
+// @version      3.7.0
 // @description  Adds relevant YouTube videos to Wikipedia & 百度百科
 // @description:zh-cn  Adds relevant YouTube videos to 维基百科 & 百度百科
 // @description:zh-TW  Adds relevant YouTube videos to 維基百科 & 百度百科
@@ -21,21 +21,18 @@
 // @require      https://github.com/johan456789/userscripts/raw/main/utils/logger.js
 // ==/UserScript==
 
-$(document).ready(function () {
-    const logger = Logger('[Wikitube]');
+const Wikitube = (function(){
+	const logger = Logger('[Wikitube]');
+	const CACHE_EXPIRY = 30 * 24 * 60 * 60 * 1000; // 30 days
 
-    // check api key validity: https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&key=YOUR_API_KEY
-    let YOUTUBE_DATA_API_CREDENTIAL = GM_getValue("youtubeApiKey", "");
-    
-    if (!YOUTUBE_DATA_API_CREDENTIAL) {
-        YOUTUBE_DATA_API_CREDENTIAL = prompt(
-            '[Wikitube] YouTube API key not set. Please enter your YouTube Data API v3 key:',
-            ''
-        );
-        if (YOUTUBE_DATA_API_CREDENTIAL) {
-            GM_setValue("youtubeApiKey", YOUTUBE_DATA_API_CREDENTIAL);
-        }
-    }
+	const state = {
+		titleText: '',
+		numVideosToLoad: 0,
+		numVideosLoaded: 0,
+		container: null,
+		moreButton: null,
+		apiKey: ''
+	};
 
 	function addGlobalStyle(css) {
 		let head, style;
@@ -47,195 +44,170 @@ $(document).ready(function () {
 		head.appendChild(style);
 	}
 
-	addGlobalStyle('#wikitube_container { padding-bottom: 30px; overflow-y:hidden; white-space: nowrap; }');
-	addGlobalStyle('#wikitube_container div { position: relative; width: auto; height: 200px; margin-right: 5px; display: inline-block; box-shadow: 0 0 5px #888; }');
-	addGlobalStyle('#wikitube_container .plusBtn { width: 100px;\ttext-align: center;\tborder-radius: 5px;\tbackground-color: rgb(192, 62, 62);\tbackground-position: center;\tbackground-repeat: no-repeat;\tbackground-size: 64px 64px;\tcursor: pointer;}');
-	addGlobalStyle('#wikitube_container .plusBtn:hover { background-color: rgb(192, 92, 92); }');
+	function injectBaseStyles() {
+		addGlobalStyle('#wikitube_container { padding-bottom: 30px; overflow-y:hidden; white-space: nowrap; }');
+		addGlobalStyle('#wikitube_container div { position: relative; width: auto; height: 200px; margin-right: 5px; display: inline-block; box-shadow: 0 0 5px #888; }');
+		addGlobalStyle('#wikitube_container .plusBtn { width: 100px;\ttext-align: center;\tborder-radius: 5px;\tbackground-color: rgb(192, 62, 62);\tbackground-position: center;\tbackground-repeat: no-repeat;\tbackground-size: 64px 64px;\tcursor: pointer;}');
+		addGlobalStyle('#wikitube_container .plusBtn:hover { background-color: rgb(192, 92, 92); }');
+	}
 
-	const allow_path = function(path){
-        logger('Processing path:', path);
-        const host = window.location.hostname;
-        let articleTitle = null;
+	function getApiKey() {
+		let key = GM_getValue('youtubeApiKey', '');
+		if (!key) {
+			key = prompt('[Wikitube] YouTube API key not set. Please enter your YouTube Data API v3 key:', '');
+			if (key) {
+				GM_setValue('youtubeApiKey', key);
+			}
+		}
+		return key;
+	}
 
-		// get article title
-        const onWikipediaIndex = /\.wikipedia\.org$/.test(host) && path === '/w/index.php';
-        if (onWikipediaIndex) {
-            articleTitle = new URLSearchParams(window.location.search).get('title');
-            if (!articleTitle) {
-                return false;
-            }
-        } else {
-            const prefixes = ['/wiki/', ...wikipedia_lang_codes.map(lang => '/' + lang + '/')];
-            for (const prefix of prefixes) {
-                if (path.startsWith(prefix)) {
-                    articleTitle = path.substring(prefix.length);
-                    break;
-                }
-            }
-        }
+	function isAllowedPath(path) {
+		logger('Processing path:', path);
+		const host = window.location.hostname;
+		let articleTitle = null;
 
-		// validate article title
-		if (!articleTitle) {
-			return false;
+		const onWikipediaIndex = /\.wikipedia\.org$/.test(host) && path === '/w/index.php';
+		if (onWikipediaIndex) {
+			articleTitle = new URLSearchParams(window.location.search).get('title');
+			if (!articleTitle) { return false; }
 		} else {
-			// pages of wikipedia which should disable Wikitube
-			const banned_title_prefixes = [
-				'Help:',
-				'Wikipedia:',
-				'User:',
-				'Special:'
-			];
-			const banned_titles = [
-				'Main_Page',
-			];
-            for (let i = 0; i < banned_title_prefixes.length; i++) {
-                if (articleTitle.startsWith(banned_title_prefixes[i])) {
-                    return false;
-                }
-            }
-            for (let i = 0; i < banned_titles.length; i++) {
-                if (articleTitle === banned_titles[i]) {
-                    return false;
-                }
-            }
-        }
+			const prefixes = ['/wiki/', ...wikipedia_lang_codes.map(function(lang){ return '/' + lang + '/'; })];
+			for (let i = 0; i < prefixes.length; i++) {
+				const prefix = prefixes[i];
+				if (path.indexOf(prefix) === 0) {
+					articleTitle = path.substring(prefix.length);
+					break;
+				}
+			}
+		}
+
+		if (!articleTitle) { return false; }
+		const banned_title_prefixes = ['Help:', 'Wikipedia:', 'User:', 'Special:'];
+		const banned_titles = ['Main_Page'];
+		for (let i = 0; i < banned_title_prefixes.length; i++) {
+			if (articleTitle.indexOf(banned_title_prefixes[i]) === 0) { return false; }
+		}
+		for (let j = 0; j < banned_titles.length; j++) {
+			if (articleTitle === banned_titles[j]) { return false; }
+		}
 		return true;
 	}
 
-	let title_text;
-	let num_videos_to_load;
-	let num_videos_loaded = 0;
-	const more_videos_button = $('<div class="plusBtn" title="Load more videos!"></div>');
-	const container = $('<div id="wikitube_container"></div>');
-
-	const first_load = function(){
-        if( $('#mw-content-text').length ){ // wikipedia
-            container.insertBefore('#mw-content-text');
-        } else if( $('.main-content').length ){ // 百度百科
-            container.insertBefore('.main-content');
-        } else if ($('#fullContent').length) { // wikiwand
-            container.insertBefore('#fullContent');
-        }
-		container.append(more_videos_button);
-
-		const plusSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><rect x="56" y="24" width="16" height="80" rx="8" fill="white"/><rect x="24" y="56" width="80" height="16" rx="8" fill="white"/></svg>';
-		const plusSvgURL = 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(plusSvg);
-
-		more_videos_button.css('background-image', 'url(' + plusSvgURL + ')');
-
-		more_videos_button.click(function(){
-			load_new_videos(false);
-		});
-
-        $('iframe').ready(function(){
-            vinc_set_horiz_scroll();
-        });
+	function determineContext() {
+		if ($('#mw-content-text').length) {
+			return {
+				titleText: $("#firstHeading")[0].textContent,
+				numVideosToLoad: Math.floor($('#bodyContent').width() / 350) + 1,
+				insertBefore: '#mw-content-text'
+			};
+		} else if ($('.main-content').length) {
+			return {
+				titleText: $(".lemmaWgt-lemmaTitle-title h1")[0].textContent,
+				numVideosToLoad: Math.floor($('.body-wrapper .content-wrapper .content').width() / 350) + 1,
+				insertBefore: '.main-content'
+			};
+		} else if ($('#fullContent').length) {
+			return {
+				titleText: $('.firstHeading > span').text(),
+				numVideosToLoad: Math.floor($('#article_content_wrapper').width() / 350),
+				insertBefore: '#fullContent'
+			};
+		}
+		return null;
 	}
-
-	// Cache helper functions
-	const CACHE_EXPIRY = 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
 
 	function getCachedResponse(key) {
 		const cached = GM_getValue(key);
 		if (!cached) return null;
-
-		const {timestamp, data} = JSON.parse(cached);
-		if (Date.now() - timestamp > CACHE_EXPIRY) {
-			GM_deleteValue(key);
+		try {
+			const obj = JSON.parse(cached);
+			if (!obj || !obj.timestamp) { return null; }
+			if (Date.now() - obj.timestamp > CACHE_EXPIRY) {
+				if (typeof GM_deleteValue === 'function') { GM_deleteValue(key); }
+				return null;
+			}
+			return obj.data;
+		} catch(e){
 			return null;
 		}
-		return data;
 	}
 
 	function setCachedResponse(key, data) {
-		const cacheObj = {
-			timestamp: Date.now(),
-			data: data
-		};
+		const cacheObj = { timestamp: Date.now(), data: data };
 		GM_setValue(key, JSON.stringify(cacheObj));
 	}
 
-	const load_new_videos = function(is_first_load){
-		const cacheKey = 'yt_search_' + title_text;
+	function setHorizScroll(){
+		$('#wikitube_container').on('mousewheel DOMMouseScroll', function(e){
+			let delt = null;
+			if (e.type == 'mousewheel') { delt = (e.originalEvent.wheelDelta * -1); }
+			else if (e.type == 'DOMMouseScroll') { delt = 40 * e.originalEvent.detail; }
+			if (delt) { e.preventDefault(); $(this).scrollLeft(delt + $(this).scrollLeft()); }
+		});
+	}
+
+	function setupContainer(insertBeforeSelector){
+		state.container = $('<div id="wikitube_container"></div>');
+		state.moreButton = $('<div class="plusBtn" title="Load more videos!"></div>');
+		state.container.insertBefore(insertBeforeSelector);
+		state.container.append(state.moreButton);
+		const plusSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><rect x="56" y="24" width="16" height="80" rx="8" fill="white"/><rect x="24" y="56" width="80" height="16" rx="8" fill="white"/></svg>';
+		const plusSvgURL = 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(plusSvg);
+		state.moreButton.css('background-image', 'url(' + plusSvgURL + ')');
+		state.moreButton.click(function(){ loadAndRender(false); });
+		$('iframe').ready(function(){ setHorizScroll(); });
+	}
+
+	function addVideosToPage(newVideos){
+		for (let i = 0; i < newVideos.length; i++) {
+			const video = newVideos[i];
+			const videoHtml = '<div class="vinc_yt"><iframe width="350" height="200" frameborder="0" allowfullscreen src="//www.youtube.com/embed/'+video['id']['videoId']+'"></iframe></div>';
+			state.moreButton.before(videoHtml);
+		}
+	}
+
+	let context = null;
+
+	function loadAndRender(isFirstLoad){
+		const cacheKey = 'yt_search_' + state.titleText;
 		const cachedItems = getCachedResponse(cacheKey);
-
-		function processVideos(videoItems) {
-			if (is_first_load) {
-				first_load();
-			}
-			let videos = videoItems;
-			const new_videos = videos.slice(num_videos_loaded);
-			num_videos_loaded += num_videos_to_load;
-			add_videos_to_page(new_videos);
+		function processVideos(videoItems){
+			if (isFirstLoad) { setupContainer(context.insertBefore); }
+			const newVideos = videoItems.slice(state.numVideosLoaded);
+			state.numVideosLoaded += state.numVideosToLoad;
+			addVideosToPage(newVideos);
 		}
-
-		if (cachedItems) {
-			processVideos(cachedItems);
-			return;
-		}
-
-		const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+title_text+'&key=' + YOUTUBE_DATA_API_CREDENTIAL + '&maxResults='+(num_videos_loaded+num_videos_to_load);
+		if (cachedItems) { processVideos(cachedItems); return; }
+		const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + encodeURIComponent(state.titleText) + '&key=' + state.apiKey + '&maxResults=' + (state.numVideosLoaded + state.numVideosToLoad);
 		$.getJSON(url, function(response){
-			if(response['items'].length > 0){
+			if (response && response['items'] && response['items'].length > 0){
 				setCachedResponse(cacheKey, response['items']);
 				processVideos(response['items']);
 			}
 		});
 	}
 
-	const add_videos_to_page = function(new_videos){
-		for (let i = 0; i < new_videos.length; i++) {
-			let video = new_videos[i];
-			const videoHtml = '<div class="vinc_yt"><iframe width="350" height="200" frameborder="0" allowfullscreen src="//www.youtube.com/embed/'+video['id']['videoId']+'"></iframe></div>';
-			more_videos_button.before(videoHtml);
-		};
+	function init(){
+		state.apiKey = getApiKey();
+		injectBaseStyles();
+		if (!isAllowedPath(window.location.pathname)) { return; }
+		context = determineContext();
+		if (!context) { 
+			logger('No context found');
+			return; 
+		}
+		// insert placeholder container
+		state.titleText = context.titleText;
+		state.numVideosToLoad = context.numVideosToLoad;
+		loadAndRender(true);
 	}
 
-	const test_func = function(){
-		let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=memes&key=' + YOUTUBE_DATA_API_CREDENTIAL;
-		$.getJSON(url, function(response){
-			logger('Test response:', response);
-		})
-	}
+	return { init: init };
+})();
 
-    const vinc_set_horiz_scroll = function(){
-        $('#wikitube_container').on('mousewheel DOMMouseScroll', function(e){
-            let delt = null;
-
-            if (e.type == 'mousewheel') {
-                delt = (e.originalEvent.wheelDelta * -1);
-            }
-            else if (e.type == 'DOMMouseScroll') {
-                delt = 40 * e.originalEvent.detail;
-            }
-
-            if (delt) {
-                e.preventDefault();
-                $(this).scrollLeft(delt + $(this).scrollLeft());
-            }
-        });
-    }
-
-    // main code
-    if(allow_path(window.location.pathname)){
-        if( $('#mw-content-text').length ){ // wikipedia
-
-            // title_text = document.getElementById('firstHeading').innerText;
-            title_text = $("#firstHeading")[0].textContent;
-            num_videos_to_load = Math.floor($('#bodyContent').width() / 350) + 1; //video width = 350px
-
-            // [REMOVED] append a youtube link icon after title
-        } else if( $('.main-content').length ){ // 百度百科
-            title_text = $(".lemmaWgt-lemmaTitle-title h1")[0].textContent;
-            num_videos_to_load = Math.floor($('.body-wrapper .content-wrapper .content').width() / 350) + 1; //video width = 350px
-
-        } else if ($('#fullContent').length){ // wikiwand
-            title_text = $('.firstHeading > span').text();
-            num_videos_to_load = Math.floor($('#article_content_wrapper').width() / 350); //video width = 350px
-        }
-        load_new_videos(true);
-    }
-
+$(document).ready(function () {
+	Wikitube.init();
 });
 
 // https://github.com/wikimedia/language-data/blob/master/data/langdb.yaml
